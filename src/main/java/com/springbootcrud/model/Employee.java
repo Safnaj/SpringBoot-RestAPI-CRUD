8c6047664668ac345c9911f0b7fd786bb3032684
Safnaj <safnaj99@gmail.com>
2019-05-18 19:51:37 +0530

    Insert Error Fixed

--- src/main/java/com/springbootcrud/model/Employee.java
+++ src/main/java/com/springbootcrud/model/Employee.java
@@ -44,11 +44,11 @@ public class Employee {
 	@Column(name = "createdAt")
 	private Date createdAt;
 	
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
